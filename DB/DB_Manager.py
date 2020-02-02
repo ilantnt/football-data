007b8a3dee334719c828580133a3d50f6ed3a2d6
ilantnt <ilantent1906@gmail.com>
2020-02-03 00:35:33 +0200

    db config change
    changed db and tables config

--- DB/DB_Manager.py
+++ DB/DB_Manager.py
@@ -26,15 +26,15 @@ class DB_handler:
         self.cursor = self.DBManager.cursor()
 
     def insert_db(self,player_info):
-        print(type(player_info.name))
+        print(player_info.name)
         sql_query= "INSERT INTO players_data VALUES ({},{},{},{},{},{},{})".format(
                                     "'"+player_info.name+"'",
                                     "'team a'",
                                     "'postion b'",
-                                    "'"+player_info.goals+ "'",
-                                    "'"+player_info.assists+ "'",
-                                    "'"+player_info.apps+ "'",
-                                    "'"+player_info.rating+ "'")
-        print("sql_query-",sql_query)
+                                    0 if player_info.goals=="-" else player_info.goals,
+                                    0 if player_info.assists=="-" else player_info.assists,
+                                    0 if player_info.apps=="-" else player_info.apps,
+                                    player_info.rating)
+        print(sql_query)
         self.cursor.execute(sql_query)
         self.DBManager.commit()
